48bba885033397d41ba98c30f9e6103ceaa4a160
JoaoMateus98 <joaobsu98@gmail.com>
2023-10-21 20:12:10 -0400

    better movement

--- bullet.py
+++ bullet.py
@@ -3,18 +3,18 @@ import math
 
 
 class Bullet:
-    def __init__(self, screen: pygame.Surface, initial_pos: dict, trajectory_radians):
+    def __init__(self, screen: pygame.Surface, initial_x, initial_y, trajectory_radians):
         self.__screen = screen
-        self.current_pos = {"x": initial_pos["x"],
-                            "y": initial_pos["y"]}
+        self.current_x = initial_x
+        self.current_y = initial_y
         self.__trajectory_radians = trajectory_radians
 
     def fire(self):
-        bullet_speed = 10
-        self.current_pos["x"] += (math.cos(self.__trajectory_radians) * bullet_speed)
-        self.current_pos["y"] += (-1 * math.sin(self.__trajectory_radians) * bullet_speed)
+        bullet_speed = 15
+        self.current_x += (math.cos(self.__trajectory_radians) * bullet_speed)
+        self.current_y += (-1 * math.sin(self.__trajectory_radians) * bullet_speed)
 
-        center = (self.current_pos["x"], self.current_pos["y"])
+        center = (self.current_x, self.current_y)
         Bullet.__draw_bullet(self, center)
 
     def __draw_bullet(self, center):
--- player.py
+++ player.py
@@ -1,52 +1,48 @@
 import math
 import pygame
-from acceleration import Acceleration
 from bullet import Bullet
 
 
 class Player:
     def __init__(self, screen: pygame.Surface, size: int):
         self.screen = screen
-        self.center = {"x": self.screen.get_width() / 2,
-                       "y": self.screen.get_height() / 2}
+        self.x = self.screen.get_width() / 2
+        self.y = self.screen.get_height() / 2
+        self.dx = 0
+        self.dy = 0
+
         self.size = size
         self.rotation_radians = math.pi / 2
-        self.head = {"x": self.center["x"] + (math.cos(self.rotation_radians) * self.size),
-                     "y": self.center["y"] + (-1 * (math.sin(self.rotation_radians) * self.size))}
-
-        self.acceleration_module = Acceleration()
-        self.rotation_radians_during_acceleration = self.rotation_radians
-        self.velocity = 0
+        self.head_x = self.x + (math.cos(self.rotation_radians) * self.size)
+        self.head_y = self.y + (-1 * (math.sin(self.rotation_radians) * self.size))
 
         self.was_firing = False
         self.fired_bullets = []
 
     def draw_player(self) -> pygame.Rect:
-
+        Player.__stay_in_screen(self)
         Player.__movement_controller(self)
         player_width = 2
 
         player_left_degrees = self.rotation_radians + ((5 * math.pi) / 6)
         player_right_degrees = self.rotation_radians + ((7 * math.pi) / 6)
 
-        player_left = {"x": self.center["x"] + (math.cos(player_left_degrees) * self.size),
-                       "y": self.center["y"] + (-1 * (math.sin(player_left_degrees) * self.size))}
-
-        player_right = {"x": self.center["x"] + (math.cos(player_right_degrees) * self.size),
-                        "y": self.center["y"] + (-1 * (math.sin(player_right_degrees) * self.size))}
+        player_left_x = self.x + (math.cos(player_left_degrees) * self.size)
+        player_left_y = self.y + (-1 * (math.sin(player_left_degrees) * self.size))
+        player_right_x = self.x + (math.cos(player_right_degrees) * self.size)
+        player_right_y = self.y + (-1 * (math.sin(player_right_degrees) * self.size))
 
         player = pygame.draw.lines(self.screen, color="white", closed=True,
-                                   points=[(player_left["x"], player_left["y"]),
-                                           (self.center["x"], self.center["y"]),
-                                           (player_right["x"], player_right["y"]),
-                                           (self.head["x"], self.head["y"])],
+                                   points=[(player_left_x, player_left_y),
+                                           (self.x, self.y),
+                                           (player_right_x, player_right_y),
+                                           (self.head_x, self.head_y)],
                                    width=player_width)
 
         return player
 
     def __movement_controller(self):
         # normalize the radians
-
         if self.rotation_radians > (math.pi * 2):
             self.rotation_radians = 0
         if self.rotation_radians < 0:
@@ -58,11 +54,22 @@ class Player:
         Player.__rotate_player(self, keys)
         Player.__move_forward(self, keys)
 
+    def __stay_in_screen(self):
+        if self.x < 0:
+            self.x = self.screen.get_width()
+        if self.x > self.screen.get_width():
+            self.x = 0
+
+        if self.y < 0:
+            self.y = self.screen.get_height()
+        if self.y > self.screen.get_height():
+            self.y = 0
+
     def __bullet_controller(self, keys):
         for index, bullet in enumerate(self.fired_bullets):
             bullet.fire()
-            if (bullet.current_pos["x"] > self.screen.get_width() or bullet.current_pos["x"] < 0 or
-                    bullet.current_pos["y"] > self.screen.get_height() or bullet.current_pos["y"] < 0):
+            if (bullet.current_x > self.screen.get_width() or bullet.current_x < 0 or
+                    bullet.current_y > self.screen.get_height() or bullet.current_y < 0):
                 self.fired_bullets.pop(index)
 
         if not self.was_firing:
@@ -75,7 +82,7 @@ class Player:
             self.was_firing = False
 
     def __fire(self):
-        bullet = Bullet(self.screen, self.head, self.rotation_radians)
+        bullet = Bullet(self.screen, self.head_x, self.head_y, self.rotation_radians)
         self.fired_bullets.append(bullet)
 
     def __rotate_player(self, keys):
@@ -83,30 +90,45 @@ class Player:
 
         if keys[pygame.K_RIGHT]:
             self.rotation_radians = self.rotation_radians - math.radians(rotation_speed)
-            self.head["x"] = self.center["x"] + (math.cos(self.rotation_radians) * self.size)
-            self.head["y"] = self.center["y"] + (-1 * math.sin(self.rotation_radians) * self.size)
+            self.head_x = self.x + (math.cos(self.rotation_radians) * self.size)
+            self.head_y = self.y + (-1 * math.sin(self.rotation_radians) * self.size)
 
         if keys[pygame.K_LEFT]:
             self.rotation_radians = self.rotation_radians + math.radians(rotation_speed)
-            self.head["x"] = self.center["x"] + (math.cos(self.rotation_radians) * self.size)
-            self.head["y"] = self.center["y"] + (-1 * math.sin(self.rotation_radians) * self.size)
+            self.head_x = self.x + (math.cos(self.rotation_radians) * self.size)
+            self.head_y = self.y + (-1 * math.sin(self.rotation_radians) * self.size)
 
     def __move_forward(self, keys):
+        self.x += self.dx
+        self.y += self.dy
 
         if keys[pygame.K_UP]:
-            self.velocity = self.acceleration_module.accelerate()
-
-            self.center["x"] = self.center["x"] + (math.cos(self.rotation_radians) * self.velocity)
-            self.center["y"] = self.center["y"] + (-1 * math.sin(self.rotation_radians) * self.velocity)
-
-            # record last radians so ship keeps the same momentum during deceleration
-            self.rotation_radians_during_acceleration = self.rotation_radians
+            Player.accelerate(self)
         else:
-            self.velocity = self.acceleration_module.decelerate()
+            Player.decelerate(self)
 
-            self.center["x"] = self.center["x"] + (math.cos(self.rotation_radians_during_acceleration) * self.velocity)
-            self.center["y"] = (self.center["y"] +
-                                (-1 * math.sin(self.rotation_radians_during_acceleration) * self.velocity))
+        self.head_x = self.x + (math.cos(self.rotation_radians) * self.size)
+        self.head_y = self.y + (-1 * math.sin(self.rotation_radians) * self.size)
 
-        self.head["x"] = self.center["x"] + (math.cos(self.rotation_radians) * self.size)
-        self.head["y"] = self.center["y"] + (-1 * math.sin(self.rotation_radians) * self.size)
+    def accelerate(self):
+        acceleration_scale = .3
+
+        if abs(self.dx) < 10:
+            self.dx += math.cos(self.rotation_radians) * acceleration_scale
+        if abs(self.dy) < 10:
+            self.dy += (-1 * math.sin(self.rotation_radians)) * acceleration_scale
+
+    def decelerate(self):
+
+        if -.1 < self.dx < .1:
+            self.dx = 0
+        elif self.dx < .1:
+            self.dx += .05
+        elif self.dx > .1:
+            self.dx -= .05
+        if -.1 < self.dy < .1:
+            self.dy = 0
+        elif self.dy < .1:
+            self.dy += .05
+        elif self.dy > .1:
+            self.dy -= .05
